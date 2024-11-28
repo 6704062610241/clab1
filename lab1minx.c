7527e3e81703e868d6f6bb16a8e5fdd479dd2f49
6704062610241 <s6704062610241@email.kmutnb.ac.th>
2024-11-28 14:55:01 +0700

    ข้อ 4 n1 n2 อยู่คนละบรรทัด

--- lab1minx.c
+++ lab1minx.c
@@ -2,12 +2,14 @@
 int main() {
 	int num1;
 	int num2;
-	printf("n1 n2");
-	scanf("%d %d", &num1 ,&num2);
+	printf("n1");
+	scanf("%d", &num1);
+	printf("n2");
+	scanf("%d", &num2);
 	if (num1>num2){
-		printf("n1 %d greater than n2", num1-num2);
+		printf("num1 %d greater than num2", num1-num2);
 	} else{
-		printf("n2 %d greater than n1", num2-num1);
+		printf("num2 %d greater than num1", num2-num1);
 	}
 
 }
