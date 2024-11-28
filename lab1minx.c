099da36e4078f81d87856c8766639195935a554c
6704062610241 <s6704062610241@email.kmutnb.ac.th>
2024-11-28 14:51:35 +0700

    ข้อ 2 n1= n2=

--- lab1minx.c
+++ lab1minx.c
@@ -2,14 +2,14 @@
 int main() {
 	int num1;
 	int num2;
-	printf("Type a number 1 and press enter: \n");
+	printf("n1=");
 	scanf("%d", &num1);
-	printf("Type a number 2 and press enter: \n");
+	printf("n2=");
 	scanf("%d", &num2);
 	if (num1>num2){
-		printf("num1 %d greater than num2", num1-num2);
+		printf("n1 %d greater than n2", num1-num2);
 	} else{
-		printf("num2 %d greater than num1", num2-num1);
+		printf("n2 %d greater than n1", num2-num1);
 	}
 
 }
