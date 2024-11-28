d79a6c339c38b3451720d7ed408576c8f17533fc
6704062610241 <s6704062610241@email.kmutnb.ac.th>
2024-11-28 14:45:44 +0700

    Add files via upload

--- lab1minx.c
+++ lab1minx.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
 int main() {
-	int num1 = 5;
-	int num2 = 10;
+	int num1;
+	int num2;
+	printf("Type a number 1 and press enter: \n");
+	scanf("%d", &num1);
+	printf("Type a number 2 and press enter: \n");
+	scanf("%d", &num2);
 	if (num1>num2){
-		printf("num1 greater than num2");
+		printf("num1 %d greater than num2", num1-num2);
 	} else{
-		printf("num2 greater than num1");
+		printf("num2 %d greater than num1", num2-num1);
 	}
+
 }
